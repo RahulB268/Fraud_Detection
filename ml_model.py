57b87767613c1e1f866a8f91a67c7027b3762ab4
RahulB268 <Rahulkumar268@outllok.com>
2023-05-25 02:18:16 +0530

    added 3 new ml model code

--- ml_model.py
+++ ml_model.py
@@ -1,2 +1,12 @@
 
 print('adding all code related to model')
+
+def model1():
+    print('adding code for model1')
+
+def model2():
+    print('adding code for model2')
+
+def model3():
+    print('adding code for model2')
+
